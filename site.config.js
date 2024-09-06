a4e4d17833a602100376e6b260b04410f4aa54e6
Jiwoong Jeon <100078615+wldnd2@users.noreply.github.com>
2024-09-06 17:35:21 +0900

    Refactor: Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,26 +1,26 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "wldnd2",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    role: "SW developer",
+    bio: "I develop everything using C++ & Python.",
+    email: "jiwoong000628@gmail.com",
+    linkedin: "",
+    github: "wldnd2",
     instagram: "",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `-`,
+      href: "https://github.com/wldnd2",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
-    scheme: "dark", // 'light' | 'dark' | 'system'
+    title: "wldnd2-log",
+    description: "welcome to wldnd2-log!",
+    scheme: "system", // 'light' | 'dark' | 'system'
   },
 
   // CONFIG configration (required)
@@ -54,7 +54,7 @@ const CONFIG = {
     },
   },
   utterances: {
-    enable: true,
+    enable: false,
     config: {
       repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
       "issue-term": "og:title",
